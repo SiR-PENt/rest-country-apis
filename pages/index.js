cb648a108d1ac50b749ce49ecd323ab5b9ed389b
Olasunkanmi <olasunkanmiibalogun@gmail.com>
2022-07-08 11:19:15 +0100

    some ui fixes

--- pages/index.js
+++ pages/index.js
@@ -3,52 +3,61 @@ import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import {AiFillTags, AiOutlineSearch} from 'react-icons/ai'
+import {AiOutlineSearch} from 'react-icons/ai'
 import { useState } from 'react'
 import axios from 'axios'
-// import Image from 'next/image'
+
 
 
 export default function Home({data}) {
   const [countries, setCountries] = useState(data);
-  console.log(data);
-  const [value, setValue] = useState('hi')
+  // console.log(data);
+  const [value, setValue] = useState('')
   return (
     <div>
       <Container fluid>
         <Header />
         <Row>
-          <Col xs={12} md={4}>
-        <Form.Control
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          aria-describedby=""
-          className='shadow'
-          placeholder='Search for a country'
-        />
+          <Col xs={12} md={4} className="position-relative">
+          
+              <Form.Control
+                type="text"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                aria-describedby=""
+                className="shadow ps-4"
+                placeholder="Search for a country"
+              />
+
+            <AiOutlineSearch className="text-dark-grey position-absolute top-50 start-20px translate-middle fs-4" />
           </Col>
         </Row>
+
+        {countries.map((country) => {
+          let { flags, name, population, region, capital } = country;
+          const { common } = name;
+
+          return (
+            <Col className="border">
+              <div>
+                <img src={flags.png} width="100%" height="100%" />
+              </div>
+              <div>
+                <p>{common}</p>
+                <p>
+                  Population: <span>{population}</span>
+                </p>
+                <p>
+                  Region: <span>{region}</span>
+                </p>
+                <p>
+                  Capital: <span>{capital ? capital[0] : "No capital"}</span>
+                </p>
+              </div>
+            </Col>
+          );
+        })}
       </Container>
-  
-     <div>
-       {
-         countries.map(country => {
-           const {flags, population, region, subregion, capital} = country;
-           return (
-             <div>
-              <Row>
-               <Col xs={12} md={3}>
-                 <img src={flags.png}/>
-               </Col>  
-                </Row> 
-             </div>
-           )
-           
-         })
-       }
-     </div>
-  
     </div>
   );
 }
