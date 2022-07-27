acdb0aad710f741ef0431271fa8a636517fbd99f
Olasunkanmi <olasunkanmiibalogun@gmail.com>
2022-07-27 06:15:15 +0100

    implement more animations across site

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,23 +3,36 @@ import Col from 'react-bootstrap/Col';
 import Container from "react-bootstrap/Container";
 import styles from 'styles/index.module.scss'
 import {BsMoon} from 'react-icons/bs';
+import { motion } from "framer-motion";
 //import Stack from 'react-bootstrap/Stack'
 
 export default function Header() {
 
   return (
-      
-      <Row className={`${styles.h70px} px-3 align-content-center fixed-top bg-white shadow-sm`}>
-        <Col >
-          <p className="text-dark-blue fw-bold fs-5">Where in the world</p>
-        </Col>
-        <Col xs='auto'>
-         <div className="d-flex align-items-center mt-1">
-          <BsMoon className="text-dark-blue fs-6 fw-normal ms-auto me-1"/>
+    <Row
+      className={`${styles.h70px} px-3 align-content-center fixed-top bg-white shadow-sm`}
+    >
+      <Col>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay:0.5 }}
+          className="text-dark-blue fw-bold fs-5"
+        >
+          Where in the world
+        </motion.p>
+      </Col>
+      <Col xs="auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay:0.8 }}
+          className="d-flex align-items-center mt-1"
+        >
+          <BsMoon className="text-dark-blue fs-6 fw-normal ms-auto me-1" />
           <p className="text-dark-blue fs-6 my-auto fw-bold">Dark Mode</p>
-         </div>
-        </Col>
-      </Row>
-    
+        </motion.div>
+      </Col>
+    </Row>
   );
 }
--- pages/[Singlecountry]/index.js
+++ pages/[Singlecountry]/index.js
@@ -8,7 +8,8 @@ import styles from 'styles/index.module.scss'
 import styleTwo from 'styles/singlecountry.module.scss'
 import Link from "next/link";
 import { useRouter } from "next/router";
-
+import { v4 as uuidv4 } from "uuid";
+import { motion } from "framer-motion";
 
 export default function SingleCountry({data}) {
 
@@ -23,109 +24,187 @@ export default function SingleCountry({data}) {
    languages =  Object.values(languages)
 
     return (
-
       <Container fluid className="mt-6 px-5 pb-3">
         <Header />
 
-        <div
-          className={`${styles.navBack} ${styles.cursorPointer} rounded d-flex align-items-center shadow-sm px-2 py-2`}
-        >
-          <BiArrowBack className="fs-4" />
-          <Link href="/" scroll={false} passHref>
+        <Link href="/" scroll={false} passHref>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.1 }}
+            className={`${styles.navBack} ${styles.cursorPointer} rounded d-flex align-items-center shadow-sm px-2 py-2`}
+          >
+            <BiArrowBack className="fs-4" />
             <p className="my-auto fs-4 ms-2">Back</p>
-          </Link>
-        </div>
+          </motion.div>
+        </Link>
 
         <Row className="bg-white mt-5 py-2 rounded shadow-sm">
           <Col xs={12} md={6} className="">
-            <div className={`${styleTwo.imgContainer}`}>
-            <img
-              src={flags.png}
-              className={`${styles.img} rounded`}
-              width="100%"
-              height="100%"
-              alt=""
+            <motion.div
+              initial={{ opacity: 0, y:-50 }}
+              animate={{ opacity: 1, y:0 }}
+              transition={{ ease:"easeOut", duration: 0.8, delay: 1.4 }}
+              className={`${styleTwo.imgContainer}`}
+            >
+              <img
+                src={flags.png}
+                className={`${styles.img} rounded`}
+                width="100%"
+                height="100%"
+                alt=""
               />
-            </div>
+            </motion.div>
           </Col>
 
           <Col className={`${styleTwo.col2} pt-5`}>
             {/* row inside column */}
             <Row>
               {/* first column inside of the row */}
-              <p className={`${styleTwo.customFs} text-dark-blue fw-bold`}>{name.common}</p>
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 1.7 }}
+                className={`${styleTwo.customFs} text-dark-blue fw-bold`}
+              >
+                {name.common}
+              </motion.p>
 
               <Col xs={12} md={6} className={`${styleTwo.descCol1}`}>
-                <p className="text-dark-blue fs-7 fw-bold">
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 2.0 }}
+                  className="text-dark-blue fs-7 fw-bold"
+                >
                   Native Name:{" "}
                   <span className="fs-6 text-light-blue">{nativeName}</span>
-                </p>
+                </motion.p>
 
-                <p className="text-dark-blue fs-7 fw-bold">
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 2.3 }}
+                  className="text-dark-blue fs-7 fw-bold"
+                >
                   Population:{" "}
                   <span className="fs-6 text-light-blue ">{population}</span>
-                </p>
+                </motion.p>
 
-                <p className="text-dark-blue fs-7 fw-bold">
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 2.6 }}
+                  className="text-dark-blue fs-7 fw-bold"
+                >
                   Region: <span className="fs-6 text-light-blue">{region}</span>
-                </p>
-                <p className="text-dark-blue fs-7 fw-bold">
+                </motion.p>
+
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 2.9 }}
+                  className="text-dark-blue fs-7 fw-bold"
+                >
                   Sub Region:{" "}
                   <span className="fs-6 text-light-blue">{subregion}</span>
-                </p>
-                <p className="text-dark-blue fs-7 fw-bold">
+                </motion.p>
+
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 3.2 }}
+                  className="text-dark-blue fs-7 fw-bold"
+                >
                   Capital:{" "}
                   <span className="fs-6 text-light-blue">
                     {capital ? capital[0] : "No Capital"}
                   </span>
-                </p>
+                </motion.p>
               </Col>
 
               <Col md={6} className={`${styleTwo.descCol2} pt-5`}>
-                <p className="text-dark-blue fs-7 fw-bold">
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 3.5 }}
+                  className="text-dark-blue fs-7 fw-bold"
+                >
                   Top Level Domain:{" "}
                   <span className="fs-6 text-light-blue">{tld[0]}</span>
-                </p>
-                <p className="text-dark-blue fs-7 fw-bold">
+                </motion.p>
+
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 3.8 }}
+                  className="text-dark-blue fs-7 fw-bold"
+                >
                   Currencies:{" "}
                   <span className="fs-6 text-light-blue">{currencies}</span>
-                </p>
-                <p className="text-dark-blue fs-7 fw-bold">
-                  Languages:{" "}         
-                   <span className="fs-6 text-light-blue">
+                </motion.p>
+
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 4.1 }}
+                  className="text-dark-blue fs-7 fw-bold"
+                >
+                  Languages:{" "}
+                  <span className="fs-6 text-light-blue">
                     {/* if length of language is less than one return just the language, 
                     else return all languages separated with commas */}
-                    {
-                    languages.length > 0 
-                    ? languages.join(', ') 
-                    : languages.join('')
-                    }
+                    {languages.length > 0
+                      ? languages.join(", ")
+                      : languages.join("")}
                   </span>
-                
-                </p>
+                </motion.p>
               </Col>
 
               <div className="pt-4">
-                <p className={`${borders && 'd-flex flex-wrap align-items-center'} text-dark-blue fs-7 fw-bold`}>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 4.4 }}
+                  className={`${
+                    borders && "d-flex flex-wrap align-items-center"
+                  } text-dark-blue fs-7 fw-bold`}
+                >
                   Border Countries:
-                  {borders ? (
-                    borders.map((border) => (
-                      <span
-                        onClick={() => router.push(`/${border}`)}
-                        className={`${styles.cursorPointer} inline-block rounded fs-6 text-light-blue border px-2 py-1 shadow-sm ms-1 my-1`}
-                      >
-                        {border}
-                      </span>
-                    ))
+                  { borders ? 
+                      
+                      ( borders.map((border, index) => {
+                      const id = uuidv4();
+                       let delayValue = 4.4; // last delay value wad 4.4
+                       index = index + 1; // we don't want to have index starting from 0 
+                       delayValue = delayValue + (0.3 * index); //since we are increasing by 0.3
+
+                       return (
+                         <motion.span
+                           initial={{ opacity: 0 }}
+                           animate={{ opacity: 1 }}
+                           transition={{ duration: 0.5, delay: `${delayValue}` }}
+                           key={id}
+                           onClick={() => router.push(`/${border}`)}
+                           className={`${styles.cursorPointer} inline-block rounded fs-6 text-light-blue border px-2 py-1 shadow-sm ms-1 my-1`}
+                         >
+                           {border}
+                         </motion.span>
+                       );
+                    })
                   ) : (
-                    <span className="fs-6 text-light-blue ms-1 fw-bold">
+                    <motion.span
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ duration: 0.5, delay: 4.7 }}
+                      className="fs-6 text-light-blue ms-1 fw-bold"
+                    >
                       None
-                    </span>
+                    </motion.span>
                   )}
-                </p>
+                </motion.p>
               </div>
             </Row>
-
           </Col>
         </Row>
       </Container>
